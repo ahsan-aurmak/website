bdd5f71062a00202a82eb93e0136e5ea551c74ed
Ahsan Jalil <ahsan.jalil@aurmak.com>
2026-03-29 21:29:28 +0100

    all new

--- public/api.php
+++ public/api.php
@@ -11,14 +11,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 // CONFIGURATION
 // Load local SMTP configuration (DO NOT COMMIT THE CONFIG FILE TO GIT)
-$configPath = __DIR__ . '/smtp_config.php';
+$configPath = __DIR__ . '/../smtp_config.php';
 if (!file_exists($configPath)) {
     header('Content-Type: application/json');
     http_response_code(500);
     echo json_encode(['message' => 'Server configuration error: SMTP config missing.']);
     exit;
 }
-$smtpConfig = require($configPath); 
+$smtpConfig = require_once($configPath); 
 $contactTo = 'info@aurmak.com'; 
 $careersTo = 'careers@aurmak.com';
 $fromInfo = 'AURMAK <info@aurmak.com>';
